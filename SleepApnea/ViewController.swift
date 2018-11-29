241140dbcfb4e1cc93fa3877211d34635e3bb48c
Matthew Woodruff <mwoodruff1@live.maryville.edu>
2018-11-29 13:23:59 -0600

    Completed?

--- SleepApnea/ViewController.swift
+++ SleepApnea/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var loadWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadWebView.loadRequest(URLRequest(url: URL(string: "https://www.youtube.com/embed/4JkiWvWn2aU")!))
+        
     }
 
 
